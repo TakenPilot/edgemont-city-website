393cea66900682df613ff26953f7d2eafa9c1233
Dane Stuckel <dane.stuckel@gmail.com>
2014-08-24 14:43:57 -0400

    Added index and template tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ var gulp = require('./gulp')([
     'stylesheets',
     'scripts',
     'images',
+    'index',
     'watch',
     'serve'
 ]);
 
-gulp.task('build', ['templates', 'stylesheets', 'scripts', 'images']);
+gulp.task('build', ['templates', 'stylesheets', 'scripts', 'images', 'index']);
 gulp.task('default', ['build', 'watch', 'serve']);
